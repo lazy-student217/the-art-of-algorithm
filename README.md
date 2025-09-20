39f91e7220282823f16eddda958422bf51a3b190
lazy-student217 <lazystudent217@gmail.com>
2025-09-20 21:20:25 +0800

    init: Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# The Art of Algorithms
+
+This website is used for a presentation called 'The Art of Algorithms'. 
+
+Written in Svelte and Typescript.
+
+You are free to read the source code and fork it to modify.
+
+# Disclaimer
+
+In the code of this repository, all user-end code are written by myself (with the help of tranditional language server) and no AI is used. 
+
+# LICENSE
+
+MIT License.
---
+++ src/lib/Grid.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+    import { animate, type AnimationPlaybackControlsWithThen } from "motion";
+
+    interface Props {
+        grid: number[][];
+    }
+    const { grid: grid_props }: Props = $props();
+    let grid = $state(grid_props);
+
+    let grid_span: HTMLSpanElement[][] = $state(
+        // svelte-ignore state_referenced_locally
+        Array.from({ length: grid.length }, () =>
+            Array(grid[0].length).fill(undefined),
+        ),
+    );
+
+    export function set_grid(new_grid: number[][]): void {
+        grid = new_grid;
+    }
+
+    export function set_slot(i: number, j: number, val: number): void {
+        grid[i][j] = val;
+    }
+
+    export function get_slot(i: number, j: number): number {
+        return grid[i][j];
+    }
+
+    function move_element(
+        el: HTMLElement,
+        x: number,
+        y: number,
+    ): AnimationPlaybackControlsWithThen {
+        const bounds = el.getBoundingClientRect();
+        const dx = x - bounds.x;
+        const dy = y - bounds.y;
+        return animate(el, {
+            x: dx,
+            y: dy,
+        });
+    }
+
+    async function move_element_fade(
+        el: HTMLElement,
+        x: number,
+        y: number,
+    ): Promise<void> {
+        const bounds = el.getBoundingClientRect();
+        const dx = x - bounds.x;
+        const dy = y - bounds.y;
+        await animate(el, {
+            x: dx,
+            y: dy,
+            opacity: 0.4,
+        });
+        await animate(
+            el,
+            {
+                opacity: 0,
+            },
+            { duration: 0.1 },
+        );
+    }
+
+    export async function move_copy_to_fade(
+        i1: number,
+        j1: number,
+        i2: number,
+        j2: number,
+    ): Promise<void> {
+        const span1_origin = grid_span[i1][j1];
+        const span1 = span1_origin.cloneNode(true) as HTMLSpanElement;
+        const bound1 = span1_origin.getBoundingClientRect();
+        span1.style.position = "absolute";
+        span1.style.top = `${bound1.y + window.screenY}px`;
+        span1.style.left = `${bound1.x + window.scrollX}px`;
+        document.body.appendChild(span1);
+        const span2 = grid_span[i2][j2];
+        const bounds2 = span2.getBoundingClientRect();
+        await move_element_fade(span1, bounds2.x, bounds2.y);
+        span1.remove();
+    }
+
+    export async function swap(
+        i1: number,
+        j1: number,
+        i2: number,
+        j2: number,
+    ): Promise<void> {
+        const span1 = grid_span[i1][j1];
+        const bounds1 = span1.getBoundingClientRect();
+        const span2 = grid_span[i2][j2];
+        const bounds2 = span2.getBoundingClientRect();
+        const anim1 = move_element(span1, bounds2.x, bounds2.y);
+        const anim2 = move_element(span2, bounds1.x, bounds1.y);
+        await Promise.all([anim1, anim2]);
+        const tmp = grid[i1][j1];
+        grid[i1][j1] = grid[i2][j2];
+        grid[i2][j2] = tmp;
+    }
+</script>
+
+<table class="border-separate">
+    <tbody>
+        {#each grid as row, i}
+            <tr>
+                {#each row as item, j}
+                    <td
+                        class="border-2 border-black text-center min-w-12 h-12 rounded-md"
+                    >
+                        {#key item}
+                            <span
+                                class="text-xl inline-block"
+                                bind:this={grid_span[i][j]}
+                            >
+                                {item}
+                            </span>
+                        {/key}
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
